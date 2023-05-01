3dde083306823771c40c735d26fa82b9437af125
Jon <jonwatkins86@gmail.com>
2023-05-01 04:49:30 +0100

    added example counter

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,18 @@
-import { Component, h, vNode } from "suika";
+import { Component, h, vNode, reactive } from "suika";
+import { Button } from "suika-ui"
 
 export class Home extends Component {
+  state = reactive({ 
+    count: 0 
+  })
   public render(): vNode {
     return (
-      <h1>Hello World</h1>
+      <div id="home">
+        <h1 className="title">Hello World</h1>
+        <Button onclick={() => this.state.value.count++}>
+          Count: {this.state.value.count.toString()}
+        </Button>
+      </div>
     )
   }
 }
